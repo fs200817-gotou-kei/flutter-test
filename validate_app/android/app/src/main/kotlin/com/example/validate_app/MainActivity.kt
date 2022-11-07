921f5a466c708a45b4d6d54d7423118752e54f1d
fs200817-gotou-kei <k_goto@freestyles.jp>
2022-11-08 08:03:27 +0900

    first commit for add icon and easy validate textformfield

---
+++ validate_app/android/app/src/main/kotlin/com/example/validate_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.validate_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ validate_app/lib/text_field_validate.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TextFieldValidate extends StatefulWidget {
+  const TextFieldValidate({super.key});
+
+  @override
+  State<TextFieldValidate> createState() => _TextFieldValidateState();
+}
+
+class _TextFieldValidateState extends State<TextFieldValidate> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return '入力してください';
+                }
+                return null;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {}
+                },
+                child: Row(
+                  children: [
+                    Text('オーディオ'),
+                    Icon(Icons.audiotrack, color: Colors.green, size: 30.0),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
